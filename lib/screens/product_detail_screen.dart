53844c67ebd179043c238e4c51f53315bf2ae554
sandeepdewangan <sandeepdewangan7@gmail.com>
2021-02-08 11:56:58 +0530

    Product Detail

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -13,9 +13,17 @@ class ProductDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(product.title),
       ),
-      body: Container(
-        child: Text('Product Detail Screen'),
-      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              child: Image.network(product.imgeUrl, fit: BoxFit.cover,),
+            ),
+            Text('Rs. ${product.price}', style: TextStyle(fontSize: 20, color: Colors.blueGrey),),
+            Text(product.description, style: TextStyle(fontSize: 20,),),
+          ],
+        ),
+      )
     );
   }
 }
